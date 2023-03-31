cb6445c8e0ce0521259198c21115ed57d6c9854c
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-03-31 17:01:56 +0530

    Add files via upload

--- Flask/GFG_work/Defining Views in Flask/app.py
+++ Flask/GFG_work/Defining Views in Flask/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World!"
+def geeksforrgeeks():
+    return "Hello Students"
 
 
 if __name__=='__main__':
